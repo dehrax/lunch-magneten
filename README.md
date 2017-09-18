15501294378173b2cf9671f9f0fea2b8ed52dc1e
Tobias Skarhed <dehrax@users.noreply.github.com>
2017-09-18 23:09:38 +0200

    Fetch HTML via YQL

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Okay, this repository is meant to display lunch.cf.
 
 ##Background
-The initial version of this was written about three years ago in PHP. It is now 03:18 on a friday, and this is about the third time people have contacted me about the site being down/spitting out error messages. So fuck it. Let's rewrite it from scratch. My thought is that we write only client-side code, so we are able to host the site on GitHub. Instant updates for the win! And people will be able to contribute.
+The initial version of this was written about three years ago in PHP. It is now 03:18 on a friday (that makes it saturday, technically), and this is about the third time people have contacted me about the site being down/spitting out error messages. So fuck it. Let's rewrite it from scratch. My thought is that we write only client-side code, so we are able to host the site on GitHub. Instant updates for the win! And people will be able to contribute.
 
 ##What's new?
 So I am aiming for a really basic PWA-ish type of app. Offline availability, cached requests and homescreen icons. This means that at a bare minimum you will see a loading icon once each week. And teh rest is offline. Cool.
--- script.js
+++ script.js
@@ -1,18 +1,50 @@
 init();
-
+var container = document.querySelector("#container");
+console.log(container);
 function init(){
 	//alert("Hey!");
+	
 	displayData();
 	//Check if it is a new week compared to info stored in localstorage
-	if(new Date.now() >= new Date(localstorage.getItem('expire')).getTime()){
+	if(new Date() >= new Date(window.localStorage.getItem('expire')).getTime()){
 		fetchData();
 	}
 }
 
 //Localstorage setup: expire (the date when it needs to be updated), updated (when it was last updated), data (pure info in JSON object)
 
+function getHTML(html){
+	console.log(html);
+}
+
 function fetchData(){
 	//Fetch request
+
+	//Run select * from htmlstring where url="https://gastrogate.com/rest_lunchinfo.php?id=69" via YQL (Yahoo Query Langauge)
+	var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20htmlstring%20where%20url%3D%22https%3A%2F%2Fgastrogate.com%2Frest_lunchinfo.php%3Fid%3D69%22&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&format=json";
+	var settings = {
+		method:'POST',
+
+	};
+
+	fetch(url, settings)
+		.then((response)=>{
+			if(response.ok){
+				response.json().then((data) => {
+					console.log(data.query.results.result);
+					var parser = new DOMParser();
+					var newDom = parser.parseFromString(data.query.results.result, "text/html");
+					var nodeList = newDom.querySelector("center");
+
+					var table = nodeList.firstChild.nextSibling.innerHTML;
+					console.log(newDom);
+					console.log();
+					container.append(nodeList.firstChild.nextSibling.innerHTML);
+					console.log(newDom);
+				});	
+			}
+
+		}).catch(err => {console.log(err);});
 
 	//Parse DOM tree and construct JSON object
 
